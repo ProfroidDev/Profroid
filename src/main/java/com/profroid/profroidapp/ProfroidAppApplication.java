61d16531f35a54a0ce17bbe09b612c8c10c83c1f
MattysLeduc <mattys.leduc@gmail.com>
2025-11-19 15:08:02 -0500

    initial commit

---
+++ src/main/java/com/profroid/profroidapp/ProfroidAppApplication.java
@@ -0,0 +1,13 @@
+package com.profroid.profroidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfroidAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfroidAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/profroid/profroidapp/ProfroidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profroid.profroidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfroidAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
