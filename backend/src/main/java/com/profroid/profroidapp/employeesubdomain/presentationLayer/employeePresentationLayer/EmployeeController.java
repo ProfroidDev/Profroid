17ab92ecdfb3aa9f14837fafa48a713d27bfaf7f
OlivierGingras026 <ogingras013@gmail.com>
2025-11-29 09:26:54 -0500

    Basic Config Employee

---
+++ backend/src/main/java/com/profroid/profroidapp/employeesubdomain/presentationLayer/employeePresentationLayer/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.profroid.profroidapp.employeesubdomain.presentationLayer;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/employees")
+public class EmployeeController {
+}
