a96abebeeca39ab4ac33b609794673e13ba18a9e
MattysLeduc <mattys.leduc@gmail.com>
2025-12-06 15:15:55 -0500

    Added all the error handling

--- backend/src/main/java/com/profroid/profroidapp/customersubdomain/dataAccessLayer/CustomerRepository.java
+++ backend/src/main/java/com/profroid/profroidapp/customersubdomain/dataAccessLayer/CustomerRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     Customer findCustomerByCustomerIdentifier_CustomerId(String customerIdentifier);
+    Customer findCustomerByUserId(String userId);
 }
