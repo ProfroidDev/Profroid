3313e3342b7f7612ed72d2792c1f37145a7e6f84
MattysLeduc <mattys.leduc@gmail.com>
2026-01-05 21:21:16 -0500

    Fixed tests not working

--- backend/src/test/java/com/profroid/profroidapp/PartTesting/partDataAccessLayer/PartRepositoryIntegrationTest.java
+++ backend/src/test/java/com/profroid/profroidapp/PartTesting/partDataAccessLayer/PartRepositoryIntegrationTest.java
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class PartRepositoryIntegrationTest {
 
