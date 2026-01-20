538b81c6415eab79b0f135b1f494d04acdad34a1
MattysLeduc <mattys.leduc@gmail.com>
2026-01-20 01:45:02 -0500

    Fixed deployment issue

--- backend/src/main/java/com/profroid/profroidapp/reportsubdomain/presentationLayer/BillController.java
+++ backend/src/main/java/com/profroid/profroidapp/reportsubdomain/presentationLayer/BillController.java
@@ -144,7 +144,6 @@ public class BillController {
      * Admin only
      */
     @PreAuthorize("hasAnyRole('ADMIN', 'CUSTOMER', 'TECHNICIAN')")
-    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{billId}/status")
     public ResponseEntity<BillResponseModel> updateBillStatus(
             @PathVariable String billId,
